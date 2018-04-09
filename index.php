e50202e6bfde0fdd0d1e0c651cedd10803dd4fb7
siddarth-bellur207 <siddarth.bellur0@gmail.com>
2018-04-09 15:23:22 +0530

    Added comments

--- index.php
+++ index.php
@@ -12,34 +12,34 @@ and open the template in the editor.
     <body>
         <?php
         $x=20;
-        echo $x;
+        echo $x;//using echo for output of scalar
         echo "<br>";
-        var_dump($x);
+        var_dump($x);//using var_dump() to output a scalar
         echo  "<br>";
-        print_r($x);
-        $numbers=array(10,20,30,40);
+        print_r($x);//using print_r() to output a scalar
+        $numbers=array(10,20,30,40);//creating an array
         echo "<br>";
-        print_r($numbers);
+        print_r($numbers);//printing array using print_r()
         echo "<br>";
-        var_dump($numbers);
-        $y="Hello php, how are you?";
+        var_dump($numbers);//using var_dump to output an array
+        $y="Hello php, how are you?";//initializing a string
         echo "<br>";
-        echo strlen($y);
+        echo strlen($y);//finding length of string
         echo "<br>";
-        echo str_word_count($y);
+        echo str_word_count($y);//finding number of words in string
         echo "<br>";
-        $z=strrev($y);
-        echo $z;
-        $a="CoMpUtEr";
-        $b=strtolower($a);
+        $z=strrev($y);//reversing string and saving it in another variable
+        echo $z;//output the variable holding the reverse of string
+        $a="CoMpUtEr";//initializing another string
+        $b=strtolower($a);//converting all characters of string to lower case and storing it in a variable
         echo "<br>";
-        echo $b;
+        echo $b;//output the variable holding lower case
         echo "<br>";
-        echo strtoupper($b);
+        echo strtoupper($b);//conversion of string back to upper case
         echo "<br>";
-        echo strpos("I like PHP","PHP");
+        echo strpos("I like PHP","PHP");//finding position of word "PHP" in string
         echo "<br>";
-        echo str_replace("PHP","Java","I like PHP");
+        echo str_replace("PHP","Java","I like PHP");//replacing a word with another in a string
         ?>
     </body>
 </html>
