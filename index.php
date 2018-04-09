66b7f5e5c3ac39308200e3200b289033bec8c577
siddarth-bellur207 <siddarth.bellur0@gmail.com>
2018-04-09 14:35:55 +0530

    File added

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>All About PHP</title>
+    </head>
+    <body>
+        <?php
+        $x=20;
+        echo $x;
+        echo "<br>";
+        var_dump($x);
+        echo  "<br>";
+        print_r($x);
+        $numbers=array(10,20,30,40);
+        echo "<br>";
+        print_r($numbers);
+        echo "<br>";
+        var_dump($numbers);
+        $y="Hello php, how are you?";
+        echo "<br>";
+        echo strlen($y);
+        echo "<br>";
+        echo str_word_count($y);
+        echo "<br>";
+        $z=strrev($y);
+        echo $z;
+        $a="CoMpUtEr";
+        $b=strtolower($a);
+        echo "<br>";
+        echo $b;
+        echo "<br>";
+        echo strtoupper($b);
+        echo "<br>";
+        ?>
+    </body>
+</html>
